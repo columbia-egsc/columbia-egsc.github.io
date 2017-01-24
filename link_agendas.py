d4bf5ffe30dd04bc982640796691d582b44e57dc
anandsun <anandsun@alumni.upenn.edu>
2017-01-24 14:08:32 -0500

    Add agenda, remove bad folders
    Removed spurious department folders for DR candidates which should have
    been nested; added agendas to the resources

---
+++ link_agendas.py
@@ -0,0 +1,29 @@
+import os
+import urllib
+from bs4 import BeautifulSoup as Soup
+
+# Read in HTML and find agenda section
+sourcefile = "resources.html"
+raw_page = urllib.urlopen(sourcefile).read()
+soup = Soup(raw_page, "html.parser")
+agenda_section = soup.find('ul', class_ = "agendas_header")
+
+# Remove existing links
+for link in agenda_section.findAll(True):
+    link.extract()
+
+# Generate all links
+path = "agendas/"
+for filename in reversed(os.listdir(path)):
+    if filename.endswith(".pdf"):
+        linkname = filename.split(".pdf")[0]
+        location = path + filename
+        entry = soup.new_tag("li")
+        link = soup.new_tag("a", href=location)
+        link.string = linkname
+        agenda_section.insert_after(entry)
+        entry.insert(1, link)
+
+html = soup.prettify("utf-8")
+with open(sourcefile, "wb") as file:
+    file.write(html)
